055e428a5f4c7daefe79277cd92789df909007d0
Samuel Zormeister <57213163+samuelfzormeister@users.noreply.github.com>
2026-07-02 17:15:20 +1000

    Foundation: Fix copyright headers.

--- Foundation/Runtime.subproj/Foundation.h
+++ Foundation/Runtime.subproj/Foundation.h
@@ -1,9 +1,10 @@
-//
-//  Foundation.h
-//  Foundation
-//
-//  Created by Samuel Zormeister on 2/7/26.
-//
+/*
+ * Copyright (C) 2026, Samuel Zormeister.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 
 #if !defined(__FOUNDATION_FOUNDATION__)
 #define __FOUNDATION_FOUNDATION__ 1
--- Foundation/String.subproj/NSString.h
+++ Foundation/String.subproj/NSString.h
@@ -1,9 +1,10 @@
-//
-//  NSString.h
-//  Foundation
-//
-//  Created by Samuel Zormeister on 2/7/26.
-//
+/*
+ * Copyright (C) 2026, Samuel Zormeister.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 
 #ifndef NSString_h
 #define NSString_h

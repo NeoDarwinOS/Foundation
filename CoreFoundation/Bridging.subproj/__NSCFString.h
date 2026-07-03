6d1cf0d744c31d7f7b8816da2a24f3b32b7913ab
Samuel Zormeister <57213163+samuelfzormeister@users.noreply.github.com>
2026-07-03 13:35:56 +1000

    CF: Create __NSCFString class

---
+++ CoreFoundation/Bridging.subproj/__NSCFString.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2026, Samuel Zormeister.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#if !defined (__NSCFSTRING__)
+#define __NSCFSTRING__ 1
+
+#import "__NSCFType.h"
+
+@interface __NSCFString : __NSCFType <NSCopying, NSCoding, NSSecureCoding>
+
+@end
+
+#endif /* ! __NSCFSTRING__ */
